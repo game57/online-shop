e29e39e6d23f3a9b25d939da87775835aead23a0
Masafumi Koba <ybiquitous@gmail.com>
2019-02-04 21:07:06 +0900

    [Feature] Sign in
    This implements the new feature which users can sign in to our system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :users
   get "/sign-up", to: "users#new"
+  get "/sign-in", to: "sessions#new"
+  post "/sign-in", to: "sessions#create"
+  delete "/sign-out", to: "sessions#destroy"
+  root "welcome#index"
 end
---
+++ test/system/sessions_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class SessionsTest < ApplicationSystemTestCase
+  test "sign in" do
+    user = users(:one)
+    user.assign_attributes(email: "foo@email.com", password: "secret")
+    user.save!
+
+    visit sign_in_url
+
+    assert_selector :link, "sign up", href: sign_up_path
+
+    fill_in "Email", with: "foo@email.com"
+    fill_in "Password", with: "secret"
+    click_on "Sign in"
+    assert_current_path root_path
+    assert_selector "h1", text: "Welcome"
+  end
+end
