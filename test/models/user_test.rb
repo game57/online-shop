1e6e7f7495120c6db0ea0ce4c12602661cdf1423
Masafumi Koba <ybiquitous@gmail.com>
2019-02-04 19:07:06 +0900

    New feature: Sign up

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user" do
+    user = users(:one)
+    assert user.valid?
+  end
+
+  test "invalid without email" do
+    user = users(:one)
+    user.email = ""
+    assert user.invalid?
+    assert_equal ["Email is invalid"], user.errors.full_messages_for(:email)
+  end
+
+  test "invalid with email format" do
+    user = users(:one)
+    user.email = "aaa@bbb"
+    assert user.invalid?
+    assert_equal ["Email is invalid"], user.errors.full_messages_for(:email)
+  end
 end
